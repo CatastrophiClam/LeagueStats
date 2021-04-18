94757d329fac29b50a6136984607d796b46d5e3c
Max <jdaimax@gmail.com>
2021-04-18 14:59:38 -0400

    Dynamically get version

--- champion_data.py
+++ champion_data.py
@@ -1,15 +1,22 @@
 import json
 from Scrape.element import get_response
 
-DATA_URL = "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/en_US/champion.json"
+PATCH_NUMBER_URL = "https://ddragon.leagueoflegends.com/api/versions.json"
+
+DATA_URL = "http://ddragon.leagueoflegends.com/cdn/{0}/data/en_US/champion.json"
 champ_id_map = {}
+
+
+def get_latest_patch():
+    patch_data = json.loads(get_response(PATCH_NUMBER_URL).text)
+    return patch_data[0]
 
 
 def get_champ_id_map():
     if len(champ_id_map) > 0:
         return champ_id_map
 
-    champ_data = json.loads(get_response(DATA_URL).text)["data"]
+    champ_data = json.loads(get_response(DATA_URL.format(get_latest_patch())).text)["data"]
     for champ, data in champ_data.items():
         champ_id_map[int(data["key"])] = champ
     return champ_id_map
